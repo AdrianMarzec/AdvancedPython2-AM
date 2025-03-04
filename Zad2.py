ecfc1eac3de09d02f23771d2c3fec5284bbf4f3a
AdeRiaN <126673448+AdrianMarzec@users.noreply.github.com>
2025-03-04 12:19:33 +0100

    Zad2
    zad2

---
+++ Zad2.py
@@ -0,0 +1,12 @@
+import cv2
+
+image = cv2.imread('pies.jpg')
+(h,w) = image.shape[:2]
+cv2.imshow("Original", image)
+
+(b,g,r) = image[h,w]
+print(f"Pixel przed zmianą: - Red {r}, Green {g}, Blue {b}")
+
+image[h,w] = (0,0,255)
+(b,g,r) = image[h,w]
+print(f"Pixel po zmianie - Red {r}, Green {g}, Blue {b}")
